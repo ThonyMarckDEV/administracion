b03222794239c63c0d1d3d5ebe7de8a7fad9a915
juniorChistemas <junior15062023@gmail.com>
2025-03-25 04:56:25 -0500

    Panel userController and vuew with route users

---
+++ resources/js/pages/panel/user/indexUser.vue
@@ -0,0 +1,16 @@
+<template>
+    <Head title="users"></Head>
+    <AppLayout>
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae cupiditate praesentium nemo culpa ex, cum facilis quo quasi explicabo vel et repudiandae deserunt necessitatibus officiis odio saepe recusandae! Illo, alias!</p>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
+<style lang="css" scoped></style>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Panel\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,18 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+# route group for panel
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::prefix('panel')->name('panel.')->group(function () {
+        # module users
+        Route::resource('users', UserController::class);
+    });
+});
+
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

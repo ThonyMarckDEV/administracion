3950f74eba7c860b62ae9a0f4c85643d1ac891ec
juniorChistemas <junior15062023@gmail.com>
2025-03-26 09:03:23 -0500

    braadcrumbs in the users module

--- resources/js/pages/panel/user/indexUser.vue
+++ resources/js/pages/panel/user/indexUser.vue
@@ -1,6 +1,6 @@
 <template>
     <Head title="users"></Head>
-    <AppLayout>
+    <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
                 <TableUser :user-list="principal.userList" :user-paginate="principal.paginacion" @page-change="handlePageChange"/>
@@ -13,6 +13,15 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import TableUser from './components/tableUser.vue';
 import { useUser } from '@/composables/useUser';
+import { BreadcrumbItem } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+
 
 const {principal,loadingUsers} = useUser();
 

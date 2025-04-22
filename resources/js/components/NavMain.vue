118f9dcb8cf01b5df507cfb6dcf3edfc6ebe0f24
Bryan Rebaza <briamrebaza@hotmail.com>
2025-04-21 22:06:18 -0500

    MENUBAR CHANGED (#32)
    * ok
    
    * User 2 Bryan Rebaza
    
    * Crud Part Pagination - Suppliers
    
    * ADD Edit, Create and Delete - Error permisions in all
    
    * ADD Edit, Create and Delete in suppliers - Errors fixed
    
    * ADD search in Suppliers and few other changes
    
    * ADAPTED MENUBAR
    
    * ADAPTED MENUBAR-3
    
    ---------
    
    Co-authored-by: Junior15Dev <127185004+JuniorChistemas@users.noreply.github.com>

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -13,6 +13,10 @@ import { ChevronRight } from 'lucide-vue-next';
 defineProps<{
     items: NavItem[];
     items2: NavItem[];
+    items3: NavItem[];
+    items4: NavItem[];
+    items5: NavItem[];
+    items6: NavItem[];
 }>();
 
 const page = usePage<SharedData>();
@@ -25,9 +29,67 @@ const isMenuActivo = (item: NavItem) => {
 
 <template>
     <SidebarGroup class="px-2 py-0">
-        <SidebarGroupLabel>Platform</SidebarGroupLabel>
+        <SidebarGroupLabel>Plataforma</SidebarGroupLabel>
          <SidebarMenu>
             <SidebarMenuItem v-for="item in items" :key="item.title">
+                <SidebarMenuButton as-child :is-active="item.href === page.url" :tooltip="item.title">
+                    <Link :href="item.href">
+                        <component :is="item.icon" />
+                        <span>{{ item.title }}</span>
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu> 
+        <SidebarGroupLabel>Gestión de Servicios</SidebarGroupLabel>
+         <SidebarMenu>
+            <SidebarMenuItem v-for="item in items3" :key="item.title">
+                <SidebarMenuButton as-child :is-active="item.href === page.url" :tooltip="item.title">
+                    <Link :href="item.href">
+                        <component :is="item.icon" />
+                        <span>{{ item.title }}</span>
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu>
+        <SidebarGroupLabel>Gestión de Clientes</SidebarGroupLabel>
+        <SidebarMenu>
+            <Collapsible v-for="item in items4" :key="item.title" as-child :default-open="isMenuActivo(item) || item.isActive"  class="group/collapsible" >
+                <SidebarMenuItem>
+                    <CollapsibleTrigger as-child>
+                        <SidebarMenuButton :tooltip="item.title">
+                            <component :is="item.icon" v-if="item.icon" />
+                            <span>{{ item.title }}</span>
+                            <ChevronRight class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                        </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                        <SidebarMenuSub>
+                            <SidebarMenuSubItem v-for="subItem in item.items" :key="subItem.title" >
+                                <SidebarMenuSubButton as-child :is-active="subItem.href === page.url">
+                                    <Link :href="subItem.href">
+                                        <span>{{ subItem.title }}</span>
+                                    </Link>
+                                </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                    </CollapsibleContent>
+                </SidebarMenuItem>
+            </Collapsible>
+        </SidebarMenu>
+        <SidebarGroupLabel>Gestión Proveedores</SidebarGroupLabel>
+         <SidebarMenu>
+            <SidebarMenuItem v-for="item in items5" :key="item.title">
+                <SidebarMenuButton as-child :is-active="item.href === page.url" :tooltip="item.title">
+                    <Link :href="item.href">
+                        <component :is="item.icon" />
+                        <span>{{ item.title }}</span>
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu> 
+        <SidebarGroupLabel>Reportes de Finanzas</SidebarGroupLabel>
+         <SidebarMenu>
+            <SidebarMenuItem v-for="item in items6" :key="item.title">
                 <SidebarMenuButton as-child :is-active="item.href === page.url" :tooltip="item.title">
                     <Link :href="item.href">
                         <component :is="item.icon" />

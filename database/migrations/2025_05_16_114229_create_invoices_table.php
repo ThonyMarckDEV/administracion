890cb52d3532764634625bc0c38f6728b316132b
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-06-13 01:05:25 -0500

    Corrected invoices table

--- database/migrations/2025_05_16_114229_create_invoices_table.php
+++ database/migrations/2025_05_16_114229_create_invoices_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('payment_id')->constrained('payments')->onDelete('cascade');
             $table->foreignId('series_correlative_id')->constrained()->onDelete('restrict');
-            $table->string('serie', 10);
-            $table->string('correlative', 20);
+            $table->string('serie_assigned', 10);
+            $table->string('correlative_assigned', 20);
+            $table->enum('sunat', ['anulado', 'enviado'])->nullable()->comment('Estado del comprobante ante SUNAT');
             $table->timestamps();
-            $table->unique(['serie', 'correlative']);
+            $table->unique(['serie_assigned', 'correlative_assigned']);
         });
     }
 

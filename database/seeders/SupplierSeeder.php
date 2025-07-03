c1b2eecc2299a67b8d0ca5791ea7d3b3b1fd35f3
Bryan Rebaza <briamrebaza@hotmail.com>
2025-07-03 15:00:20 -0500

    Final PR V.1 (#42)
    * ok
    
    * User 2 Bryan Rebaza
    
    * Crud Part Pagination - Suppliers
    
    * ADD Edit, Create and Delete - Error permisions in all
    
    * ADD Edit, Create and Delete in suppliers - Errors fixed
    
    * ADD search in Suppliers and few other changes
    
    * ADAPTED MENUBAR
    
    * ADAPTED MENUBAR-3
    
    * Prueba nuevo dispositivo
    
    * CHANGES OBSERVATIONS
    
    * Litte changesS
    
    * Added Notifications and solved errors
    
    * Solved errors
    
    * Added upgrades,  solved errors with dashboard, and more
    
    * changed
    
    * add render.yaml to force PHP deployment
    
    * add render.yaml to force PHP deployment1
    
    * Changes-Finals
    
    * Final
    
    ---------
    
    Co-authored-by: Junior15Dev <127185004+JuniorChistemas@users.noreply.github.com>

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -13,6 +13,6 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
-        Supplier::factory()->count(50)->create();
+        Supplier::factory()->count(10)->create();
     }
 }
